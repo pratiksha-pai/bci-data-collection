cba3e9dd356ad90631c24506e07437f604083e68
Jack Mostyn <jackmostyn@Jacks-MacBook-Pro-4.local>
2023-09-05 19:17:53 -0400

    Added some more customization to the visual elements of the GUI. Created acquisition setup window. Now able to switch between GUIs. Some formatting changes.

--- GUIs/acquisition_setup_gui.py
+++ GUIs/acquisition_setup_gui.py
@@ -1,11 +1,11 @@
-# Imports the necessary packages
+#Imports the necessary packages
 import tkinter as tk
-from GUIs import acquisition_setup_gui
+from GUIs import main_menu_gui
 
 # Stores all the properties of the windows in one place to make changes easier
 def get_window_properties(prop):
 
-    window_title = "Main Menu"
+    window_title = "Acquisition Setup"
     window_width = 500
     window_height = 500
     window_x = 200
@@ -46,36 +46,13 @@ def add_gui_elements(root):
     # Gets the button parameters
     px, py, btn_width, btn_height = get_window_properties("btn")
 
-    # Creates the button to begin acquiring data
-    def acquisition_action(root):
+    # Creates the button to cancel the setup and return to main menu
+    def cancel(root):
         root.destroy()
-        acquisition_setup_gui.open()
-    acquisition_btn = tk.Button(frame, text="Acquisition", command=lambda: acquisition_action(root), width=btn_width, height=btn_height)
-    acquisition_btn.grid(row=0, column=0, padx=px, pady=py)
+        main_menu_gui.open()
 
-    # Creates the button to manage protocols
-    def protocols_action():
-        print("protocols")
-    protocols_btn = tk.Button(frame, text="Protocols", command=protocols_action, width=btn_width, height=btn_height)
-    protocols_btn.grid(row=1, column=0, padx=px, pady=py)
-
-    # Creates the button to manage data
-    def analysis_action():
-        print("analysis")
-    analysis_btn = tk.Button(frame, text="Analysis", command=analysis_action, width=btn_width, height=btn_height)
-    analysis_btn.grid(row=2, column=0, padx=px, pady=py)
-
-    # Creates the button to manage equipment
-    def equipment_action():
-        print("equipment")
-    equipment_btn = tk.Button(frame, text="Equipment", command=equipment_action, width=btn_width, height=btn_height)
-    equipment_btn.grid(row=3, column=0, padx=px, pady=py)
-
-    # Creates the button to exit the program
-    def exit(root):
-        root.destroy()
-    exit_btn = tk.Button(frame, text="Exit", command=lambda:exit(root), width=btn_width, height=btn_height)
-    exit_btn.grid(row=4, column=0, padx=px, pady=py)
+    exit_btn = tk.Button(frame, text="Cancel", command=lambda:cancel(root), width=btn_width, height=btn_height)
+    exit_btn.grid(row=0, column=0, padx=px, pady=py)
     return
 
 # Opens the main menu -- called by other scripts
