11db518bd94af2c42e9af3d158b6c965201e8031
Jack Mostyn <jackmostyn@lawn-10-91-56-254.lawn.gatech.edu>
2023-09-14 10:53:03 -0400

    more threading tests

--- Testing/test_threading.py
+++ Testing/test_threading.py
@@ -1,37 +1,61 @@
 import multiprocessing
 import time
 
-def print_numbers():
+def process_1():
     last_time = time.time()
-    for i in range(1, 6):
-        print(f"Number: {i}")
+    sum = 0
+    count = 0
+    for i in range(1, 100):
         current_time = time.time()
         elapsed_time = current_time - last_time
-        print("num: " + str(elapsed_time))
+        count = count + 1
+        sum = sum + elapsed_time
         last_time = current_time
         time.sleep(0.001)
+    avg = sum / count
+    print("1: " + str(avg))
 
-def print_letters():
+def process_2():
     last_time = time.time()
-    for letter in 'abcdefgh':
-        print(f"Letter: {letter}")
+    sum = 0
+    count = 0
+    for i in range(1, 10000):
         current_time = time.time()
         elapsed_time = current_time - last_time
-        print("let: " + str(elapsed_time))
+        count = count + 1
+        sum = sum + elapsed_time
         last_time = current_time
         time.sleep(0.0001)
+    avg = sum / count
+    print("2: " + str(avg))
+
+def process_3():
+    last_time = time.time()
+    sum = 0
+    count = 0
+    for i in range(1, 300):
+        current_time = time.time()
+        elapsed_time = current_time - last_time
+        count = count + 1
+        sum = sum + elapsed_time
+        last_time = current_time
+        time.sleep(0.00001)
+    avg = sum / count
+    print("3: " + str(avg))
+
 
 if __name__ == '__main__':
     # Create processes
-    process1 = multiprocessing.Process(target=print_numbers)
-    process2 = multiprocessing.Process(target=print_letters)
+    process1 = multiprocessing.Process(target=process_1)
+    process2 = multiprocessing.Process(target=process_2)
+    process3 = multiprocessing.Process(target=process_3)
 
     # Start processes
     process1.start()
     process2.start()
+    process3.start()
 
     # Wait for both processes to finish
     process1.join()
     process2.join()
-
-    print("Both processes have finished.")
+    process3.join()
